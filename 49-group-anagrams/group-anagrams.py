b927541903af51155f1758676feec890309a0a71
Chinmay Karkamkar <chinmaykarkamkar1999@gmail.com>
2025-11-10 16:34:06 +0530

    Time: 19 ms (37.17%) | Memory: 22.6 MB (24.39%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,5 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs: List[str])->List[List[str]]:
         res = defaultdict(list)
 
         for s in strs:
