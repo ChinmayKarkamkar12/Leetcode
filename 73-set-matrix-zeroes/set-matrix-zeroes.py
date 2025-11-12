a384d86dc47062f29b658517dbefcb89f66426a5
Chinmay Karkamkar <chinmaykarkamkar1999@gmail.com>
2025-11-12 19:18:56 +0530

    Time: 4 ms (38.57%) | Memory: 18.4 MB (77.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,30 +1,27 @@
 class Solution:
-    def setZeroes(self, matrix):
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         rows, cols = len(matrix), len(matrix[0])
-        first_row_zero = any(matrix[0][j] == 0 for j in range(cols))
-        first_col_zero = any(matrix[i][0] == 0 for i in range(rows))
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(cols))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(rows))
 
-        # Mark zeros in first row/col
         for i in range(1, rows):
             for j in range(1, cols):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
 
-        # Zero out cells based on markers
         for i in range(1, rows):
             for j in range(1, cols):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
-        # Handle first row
-        if first_row_zero:
+        if first_row_has_zero:
             for j in range(cols):
                 matrix[0][j] = 0
 
-        # Handle first column
-        if first_col_zero:
+        if first_col_has_zero:
             for i in range(rows):
                 matrix[i][0] = 0
-
-        
